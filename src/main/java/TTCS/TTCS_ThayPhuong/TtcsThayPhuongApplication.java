a783fbbe9a732ec3d5b7d2077b3e37437ca9ba00
tienthinh28112004 <tienthinh28112004@gmail.com>
2025-03-27 22:27:07 +0700

    ttcs

---
+++ src/main/java/TTCS/TTCS_ThayPhuong/TtcsThayPhuongApplication.java
@@ -0,0 +1,13 @@
+package TTCS.TTCS_ThayPhuong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtcsThayPhuongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtcsThayPhuongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TTCS/TTCS_ThayPhuong/TtcsThayPhuongApplicationTests.java
@@ -0,0 +1,13 @@
+package TTCS.TTCS_ThayPhuong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtcsThayPhuongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
